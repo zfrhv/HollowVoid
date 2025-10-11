97d6bc7a1ee5a2cac714be1fe870f4f9cd988f50
zahar <zfrhv2010@gmail.com>
2025-10-11 09:24:13 +0300

    Initial commit

---
+++ src/main/java/zfrhv/hollowvoid/HollowVoidDataGenerator.java
@@ -0,0 +1,11 @@
+package zfrhv.hollowvoid;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HollowVoidDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
